e6a2983509794f29709786b4486123a44fa4e23e
newguyplaying <149993929+newguyplaying@users.noreply.github.com>
2025-04-16 13:03:56 +0800

    Add files via upload

--- backend/app/controllers/Customer_service_support.py
+++ backend/app/controllers/Customer_service_support.py
@@ -5,17 +5,22 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent.parent))
 import datetime
 from sqlalchemy.orm import Session
-from app.schemas.css_message import CSSMessage, CSSMessageCreate
+from schemas.css_message import CSSMessage, CSSMessageCreate
+from models.cssmessages import CSSMessageDB
+import uuid
 x=datetime.datetime.now()
 class CustomerServiceSupportController:
-    def sendMessage(db: Session, css_message: CSSMessageCreate) -> CSSMessage:
+    def sendMessage(db: Session, css_message: CSSMessageCreate) -> CSSMessageDB:
+        css_message_idg = str(uuid.uuid4())
         db_message = CSSMessage(
             css_history_id=css_message.css_history_id,
             sender_user_id=css_message.sender_user_id,
             receiver_user_id=css_message.receiver_user_id,
             text=css_message.text,
+            css_message_id=css_message_idg,
             datetime=x.strftime("%c")
         )
+        
         db.add(db_message)
         db.commit()
         db.refresh(db_message)
