7d09864d6e06c9db289e2e228663d63f4d4b7a56
newguyplaying <149993929+newguyplaying@users.noreply.github.com>
2025-04-16 13:06:41 +0800

    Add files via upload

--- backend/app/Adding_client_testing_only.py
+++ backend/app/Adding_client_testing_only.py
@@ -2,7 +2,7 @@ import websockets as wbs
 import asyncio
 import json
 async def client():
-    uri = "ws://192.168.168.1:8080"
+    uri = "ws://localhost:8080/ws/test123"
     async with wbs.connect(uri) as test1:
         await test1.send("Hello, Server!")
         response = await test1.recv()
@@ -10,12 +10,11 @@ async def client():
 
 #Testing messaging the server:
 async def sendmsg():
-    uri = "ws://192.168.168.1:8080"
+    uri = "ws://localhost:8080/ws/test1232"
     async with wbs.connect(uri) as msg1:
-          message = {
+        message = {
             "type": "new_message",
             "css_message": {
-                "type": "new_message",
                 "css_history_id": "xyz",
                 "sender_user_id": "1",
                 "receiver_user_id": "2",
@@ -23,14 +22,27 @@ async def sendmsg():
 
             },
         }
-    await msg1.send(message)
-    print("Message sent")
+        await msg1.send(json.dumps(message))
+        print("First Message sent")
+        runchoice=1
+        while runchoice==1:
+            await asyncio.sleep(5)  # Adjust the interval as needed
+            message = {
+                "type": "new_message",
+                "css_message": {
+                    "css_history_id": "xyz",
+                    "sender_user_id": "1",
+                    "receiver_user_id": "2",
+                    "text": "Another message from backend",
+                },
+            }
+            await msg1.send(json.dumps(message))
+            print("Another message sent")
+            runchoice=int(input("1 to continue, 0 to exit"))
+        print("disconnecting...")
 
 
 
 #asyncio.run(client())
-runchoice=1
-while runchoice==1:
-    asyncio.run(sendmsg())
-    runchoice=int(input("1 to continue, 0 to exit:"))
+asyncio.run(sendmsg())
 print("Exiting....")
